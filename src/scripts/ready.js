63f03e0ac0953fefb3ca1df35d0bcef64aed6d14
Michael Cavalea <callmecavs@gmail.com>
2015-04-08 23:27:09 -0400

    add offset helper

--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -21,7 +21,7 @@ function Layzr( options ) {
 // DEBOUNCE METHODS
 // adapted from: http://www.html5rocks.com/en/tutorials/speed/animations/
 
-Layzr.prototype._updateScroll = function() {
+Layzr.prototype._requestScroll = function() {
   // TODO: make sure we're scrolling down, not up?
   this._lastScroll = window.scrollY;
   this._requestTick();
@@ -42,7 +42,20 @@ Layzr.prototype._create = function() {
   window.addEventListener('resize', this._requestScroll.bind(this), false);
 }
 
+// offset helper
+// borrow from: http://stackoverflow.com/questions/5598743/finding-elements-position-relative-to-the-document
 
+Layzr.prototype._getOffset = function( element ) {
+  var offsetTop  = 0;
+
+  do {
+    if(!isNaN(element.offsetTop)) {
+      offsetTop  += element.offsetTop;
+    }
+  } while (element = element.offsetParent);
+
+  return offsetTop;
+}
 
 Layzr.prototype.update = function() {
   // cache image NodeList length
