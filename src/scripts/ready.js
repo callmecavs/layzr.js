7ee81a0eed28938597cff33a004bd05ea5c0b980
Michael Cavalea <callmecavs@gmail.com>
2015-04-10 23:05:00 -0400

    outset

---
+++ src/scripts/ready.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// CONSTRUCTOR
+
+function Outset(options) {
+
+  document.addEventListener('DOMContentLoaded', this.init(), false);
+}
+
+// METHODS
+
+Outset.prototype.init = function() {
+
+}
+
+// INSTANCE
+
+var outset = new Outset({
+
+});
