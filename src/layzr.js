40eae75266b7ad1410e2f7c07761944cc59221cb
Michael Cavalea <callmecavs@gmail.com>
2016-01-14 15:10:05 -0500

    add options handling, and extended instance

--- src/layzr.js
+++ src/layzr.js
@@ -1,5 +1,23 @@
-export default class Layzr {
-  constructor(options = {}) {
+import knot from 'knot.js'
 
-  }
+export default (options = {}) => {
+  let prevLoc
+  let ticking
+
+  // options
+  const container  = document.querySelector(options.container) || window
+  const selector   = options.selector || '[data-layzr]'
+  const attr       = options.attr || 'data-layzr'
+  const attrRetina = options.attrRetina || 'data-layzr-retina'
+  const attrBg     = options.attrBg || 'data-layzr-bg'
+  const threshold  = options.threshold || 0
+  const callback   = options.callback
+
+  // instance
+
+  const instance = knot({
+
+  })
+
+  return instance
 }
