ec37c6a0c4f177a1b008da92aca377d76ea30104
Michael Cavalea <callmecavs@gmail.com>
2016-01-19 22:33:43 -0500

    update Layzr and images

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Layzr from './layzr'
 
 const main = () => {
+  const instance = Layzr()
 
+  instance
+    .update()
+    .handlers(true)
 }
 
 document.addEventListener('DOMContentLoaded', event => main())
